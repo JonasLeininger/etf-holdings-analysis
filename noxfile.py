7adf31dafa1c453851e9aea27a6b8f9c233a85d0
JonasLeininger <jonas.leininger@rewe-digital.com>
2021-02-28 12:35:59 +0100

    flake8 and black checks

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,53 @@
+import tempfile
+
 import nox
+
+locations = "src", "tests", "noxfile.py"
+
+
+nox.options.sessions = "lint", "tests"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
 
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
-    args = session.posargs or ["--cov"]
-    session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
+    session.run("pytest", *args)
+
+
+@nox.session(python=["3.8", "3.7"])
+def lint(session):
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.8", "3.7"])
+def black(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
--- src/etf_holdings_analysis/console.py
+++ src/etf_holdings_analysis/console.py
@@ -1,22 +1,26 @@
 import textwrap
 
 import click
-import requests
 
-from . import __version__
+from . import __version__, wikipedia
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The ETF Holdings Analysis Python Project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
-    
-    title = data['title']
-    extract = data['extract']
+    data = wikipedia.random_page(language=language)
 
-    click.secho(title, fg='green')
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
     click.echo(textwrap.fill(extract))
